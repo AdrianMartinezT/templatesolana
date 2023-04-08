a5b91f09be94cc53e67be29bde39256b2566cd34
Andres Aguilar <aaguilarc@uach.mx>
2023-04-07 20:52:02 -0500

    Uploading Version para la semana 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+
+const activeChainId = ChainId.SolanaDevnet;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <ThirdwebProvider desiredChainId={activeChainId}>
+                <Component {...pageProps} />
+            </ThirdwebProvider>
+        </>
+    );
 }
---
+++ pages/demo.js
@@ -0,0 +1,7 @@
+export default function Demo() {
+    return (
+        <div>
+            <h1>Estamos en la pagina demo</h1>
+        </div>
+    );
+}
